8e23459687f07aa8c935bbb6c041e496ef11f383
ag-mayank <117707823+ag-mayank@users.noreply.github.com>
2023-06-21 22:29:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const endDate="23 June 2024 10:10 PM" 
+document.getElementById("end-date").innerText=endDate;
+const inputs=document.querySelectorAll("input")
+function clock(){
+    const end=new Date(endDate)
+    const now=new Date()
+    console.log(end)
+    console.log(now)
+    const diff=(end-now)/1000
+    console.log(diff)
+    if(diff<0)
+        return
+    inputs[0].value=Math.floor(diff/3600/24)
+    inputs[1].value=Math.floor(diff/3600)%24
+    inputs[2].value=Math.floor((diff/60)%60)
+    inputs[3].value=Math.floor((diff)%60)
+}   
+clock()
+
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+)
